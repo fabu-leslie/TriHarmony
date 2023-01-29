d6e86127267c152b71748648469866c3e58b05de
Leslie Naylor <leslie.l.naylor@gmail.com>
2023-01-29 16:14:39 -0500

    changed behavior_data view to parent_data_form, added Specialist Form model, edit_target_behavior view

--- specialist/views.py
+++ specialist/views.py
@@ -1,66 +1,7 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
 from django.urls import reverse
-from .forms import ChildForm, BehaviorForm
+from .forms import ChildForm, BehaviorForm, SpecialistBehaviorForm
 from .models import Child, Behavior
-
-
-def home(request):
-    return render(request, 'home.html')
-
-
-def client_list(request):
-    clients = Child.objects.all()
-    return render(request, 'client_list.html', {'clients': clients})
-
-
-def add_client(request):
-    if request.method == 'POST':
-        new_client_form = ChildForm(request.POST)
-        if new_client_form.is_valid():
-            new_client_form.save()
-            return HttpResponseRedirect(reverse('specialist:client_list'))
-    else:
-        new_client_form = ChildForm()
-    return render(request, 'add_client.html', {'new_client_form': new_client_form})
-
-def client_detail(request, client_id):
-    behavior_form = BehaviorForm()
-    client = get_object_or_404(Child, pk=client_id)
-    if request.user.is_authenticated and request.user.has_perm('edit_behavior'):
-        behaviors = Behavior.objects.filter(child=client)
-        return render(request, 'client_detail.html', {'client': client, 'behaviors': behaviors})   
-    return render(request, 'client_detail.html', {'client': client, 'behavior_form': behavior_form})
-
-# View first gets client object and queries db for any behav assoc. with client
-
-
-def parent_detail(request, client_id):
-    client = get_object_or_404(Child, pk=client_id)
-    if request.user.is_authenticated and request.user.is_specialist:
-        behavior_form = BehaviorForm()
-        return render(request, 'parent_detail.html', {'client': client, 'behavior_form': behavior_form})
-    else:
-        return render(request, 'parent_detail.html', {'client': client})
-
-
-def client_behavior(request, client_id):
-    client = Child.objects.get(id=client_id)
-    behaviors = Behavior.objects.filter(child=client)
-    context = {'client': client, 'behaviors': behaviors}
-    return render(request, 'client_detail.html', context)
-
-
-def edit_behavior(request, client_id):
-    # retrieves behavior with given id from db, uses it to pre-populate a behavior form.
-    behavior = get_object_or_404(Behavior, pk=client_id)
-    if request.method == 'POST':
-        form = BehaviorForm(request.POST, instance=behavior)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('specialist:client_detail', args=(client_id,)))
-    else:
-        form = BehaviorForm(instance=behavior)
-    return render(request, 'edit_behavior.html', {'form': form, 'behavior': behavior})
 
 
 def add_behavior(request, client_id):
@@ -75,3 +16,75 @@ def add_behavior(request, client_id):
     else:
         form = BehaviorForm()
     return render(request, 'add_behavior.html', {'form': form, 'client': client})
+
+
+def add_client(request):
+    if request.method == 'POST':
+        new_client_form = ChildForm(request.POST)
+        if new_client_form.is_valid():
+            new_client_form.save()
+            return HttpResponseRedirect(reverse('specialist:client_list'))
+    else:
+        new_client_form = ChildForm()
+    return render(request, 'add_client.html', {'new_client_form': new_client_form})
+
+
+def client_behavior(request, client_id):
+    client = Child.objects.get(id=client_id)
+    behaviors = Behavior.objects.filter(child=client)
+    context = {'client': client, 'behaviors': behaviors}
+    return render(request, 'client_detail.html', context)
+
+
+def client_detail(request, client_id):
+    behavior_form = SpecialistBehaviorForm()
+    client = get_object_or_404(Child, pk=client_id)
+    if request.user.is_authenticated and request.user.has_perm('parent_data_form'):
+        behaviors = Behavior.objects.filter(child=client)
+        return render(request, 'client_detail.html', {'client': client, 'behaviors': behaviors})
+    return render(request, 'client_detail.html', {'client': client, 'behavior_form': behavior_form})
+
+
+def client_list(request):
+    clients = Child.objects.all()
+    return render(request, 'client_list.html', {'clients': clients})
+
+def edit_target_behaviors(request, client_id):
+    client = get_object_or_404(Child, id=client_id)
+    if request.method == "POST":
+        form = SpecialistBehaviorForm(request.POST, instance=client.behavior)
+        if form.is_valid():
+            form.save()
+            return redirect('client_detail', client_id)
+    else:
+        form = SpecialistBehaviorForm(instance=client.behavior)
+    return render(request, 'edit_target_behaviors.html', {'form': form, 'client': client})
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def parent_detail(request, client_id):
+    client = get_object_or_404(Child, pk=client_id)
+    if request.user.is_authenticated:
+        behavior_form = BehaviorForm()
+        return render(request, 'parent_detail.html', {'client': client, 'behavior_form': behavior_form})
+    else:
+        return render(request, 'parent_detail.html', {'client': client})
+
+
+def parent_data_form(request, client_id):
+    # retrieves behavior with given id from db, uses it to pre-populate a behavior form.
+    behavior = get_object_or_404(Behavior, pk=client_id)
+    if request.method == 'POST':
+        form = BehaviorForm(request.POST, instance=behavior)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('specialist:client_detail', args=(client_id,)))
+    else:
+        form = BehaviorForm(instance=behavior)
+    return render(request, 'parent_data_form.html', {'form': form, 'behavior': behavior})
+
+
+
